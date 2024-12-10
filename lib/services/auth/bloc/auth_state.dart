8f2c37631812b60f87e9693b5ef49084abdd5385
adeel-s <adeel4sultan@gmail.com>
2024-12-10 09:50:02 -0600

    Started implementing bloc-based authentication

---
+++ lib/services/auth/bloc/auth_state.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import 'package:mynotes/services/auth/auth_user.dart';
+
+@immutable
+abstract class AuthState {
+  const AuthState();
+}
+
+class AuthStateLoading extends AuthState {
+  const AuthStateLoading();
+}
+
+class AuthStateLoggedIn extends AuthState {
+  final AuthUser user;
+  const AuthStateLoggedIn(this.user);
+}
+
+class AuthStateLogInFailure extends AuthState {
+  final Exception
+      exception; //carry the information the process requires to complete
+  const AuthStateLogInFailure(this.exception);
+}
+
+class AuthStateNeedsVerification extends AuthState {
+  const AuthStateNeedsVerification();
+}
+
+class AuthStateLoggedOut extends AuthState {
+  const AuthStateLoggedOut();
+}
+
+class AuthStateLogOutFailure extends AuthState {
+  final Exception
+      exception; //carry the information the process requires to complete
+  const AuthStateLogOutFailure(this.exception);
+}
