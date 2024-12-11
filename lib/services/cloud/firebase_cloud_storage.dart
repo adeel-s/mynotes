e5b619a524730a7b73c4819a8fd21406450d1de2
adeel-s <adeel4sultan@gmail.com>
2024-12-11 13:34:16 -0600

    Fixed database access issue

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -25,10 +25,13 @@ class FirebaseCloudStorage {
     }
   }
 
-  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
-      notes.snapshots().map((event) => event.docs
-          .map((doc) => CloudNote.fromSnapshot(doc))
-          .where((note) => note.ownerUserId == ownerUserId));
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) {
+    final allNotes = notes
+        .where(ownerUserIdFieldName, isEqualTo: ownerUserId)
+        .snapshots()
+        .map((event) => event.docs.map((doc) => CloudNote.fromSnapshot(doc)));
+    return allNotes;
+  }
 
   Future<CloudNote> createNewNote({required String ownerUserId}) async {
     final document = await notes.add({
@@ -41,24 +44,6 @@ class FirebaseCloudStorage {
       ownerUserId: ownerUserId,
       text: '',
     );
-  }
-
-  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
-    try {
-      return await notes
-          .where(
-            ownerUserIdFieldName,
-            isEqualTo: ownerUserId,
-          )
-          .get()
-          .then(
-            (value) => value.docs.map(
-              (doc) => CloudNote.fromSnapshot((doc)),
-            ),
-          );
-    } catch (e) {
-      throw CouldNotGetAllNotesException();
-    }
   }
 
   //Forces instantializations to return the same instance of a FirebaseCloudStorage. "singleton"
